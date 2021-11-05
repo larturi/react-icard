83592224a4e7f6346f9bb0208531a9ae81d0cb4e
lea.arturi <lea.arturi@gmail.com>
2021-11-05 10:47:27 -0300

    Rutas Admin y Client react-router-dom 5.3.0

--- src/routes/Navigation.jsx
+++ src/routes/Navigation.jsx
@@ -1,36 +1,32 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { map } from 'lodash';
 import routes from './routes';
 
 export function Navigation() {
 
     return (
-        <Routes>
-            {
-                map(routes, (route, index) => (
-                    <Route 
-                        key ={index}
-                        path={route.path} 
-                        exact={route.exact}
-                        element={
-                            <route.layout>
-                                <route.component />
-                            </route.layout>
-                        }
-                    />
-                ))
-            }
-        </Routes>
+        <Router>
+            <Switch>
+                {map(routes, (route, index) => (
+                <Route
+                    key={index}
+                    path={route.path}
+                    exact={route.exact}
+                    render={(props) => (
+                    <route.layout>
+                        <route.component {...props} />
+                    </route.layout>
+                    )}
+                />
+                ))}
+            </Switch>
+        </Router>
     )
 };
 
 
-// element={(props) => (
-//     <route.layout>
-//         <route.component {...props} />
-//     </route.layout>
-// )} 
+
 
 
 
--- src/routes/routes.client.jsx
+++ src/routes/routes.client.jsx
@@ -1,6 +1,5 @@
 import { ClientLayout } from '../layouts';
 import { Home } from '../pages/ClientPages';
-import { Error404 } from '../pages';
 
 const routesClient = [
     {
@@ -8,7 +7,7 @@ const routesClient = [
         layout: ClientLayout,
         component: Home,
         exact: true
-    }
+    },
 ];
 
 export default routesClient;
