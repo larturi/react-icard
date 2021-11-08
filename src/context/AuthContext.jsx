ef4646c699af8d2268c5cafcc8e75a7b2ddf1744
lea.arturi <lea.arturi@gmail.com>
2021-11-08 18:40:59 -0300

    Logout

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
-import { setToken } from '../api/token';
+import { getToken, removeToken, setToken } from '../api/token';
 import { useUser } from '../hooks/useUser'
 
 export const AuthContext = createContext({
@@ -13,8 +13,20 @@ export function AuthProvider(props) {
 
     const { children } = props;
     const [auth, setAuth] = useState(undefined);
-
     const { getMe } = useUser();
+
+    useEffect(() => {
+        (async() => {
+            const token = getToken();
+            
+            if (token) {
+                const me = await getMe(token);
+                setAuth({ token, me });
+            } else {
+                setAuth(null)
+            }
+        })();
+    }, [getMe])
 
     const login = async (token) => {
         setToken(token);
@@ -22,11 +34,20 @@ export function AuthProvider(props) {
         setAuth({ token, me });
     };
 
+    const logout = async () => {
+        if (auth) {
+            setAuth(null);
+            removeToken();
+        }
+    };
+
     const valueContext = {
         auth,
         login,
-        logout: () => console.log('logout')
-    }
+        logout
+    };
+
+    if (auth === undefined) return null;
 
     return (
         <AuthContext.Provider value={valueContext}>
--- src/pages/AdminPages/HomeAdmin.jsx
+++ src/pages/AdminPages/HomeAdmin.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { useAuth } from '../../hooks'
+
 export const HomeAdmin = () => {
+
+    const { logout } = useAuth();
+
     return (
         <div>
             <h1>Home Admin</h1>
+            <button onClick={logout}>Cerrar sesion</button>
         </div>
     )
 }
