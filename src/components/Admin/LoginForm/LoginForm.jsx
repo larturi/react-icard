fe5a4438a8e8d3ae254ad7356b26c24e6c4a2818
lea.arturi <lea.arturi@gmail.com>
2021-11-08 10:17:04 -0300

    Yup and Formik validations LoginForm

--- src/components/Admin/LoginForm/LoginForm.jsx
+++ src/components/Admin/LoginForm/LoginForm.jsx
@@ -1,23 +1,54 @@
 import React from 'react';
-import { Button, Form, Input } from 'semantic-ui-react'
+import { Button, Form } from 'semantic-ui-react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 import './LoginForm.scss';
 
 export const LoginForm = () => {
+
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: Yup.object(validationSchema()),
+        onSubmit: (formValues) => {
+            console.log(formValues);
+        }
+    });
+
     return (
-        <Form className="login-form-admin">
+        <Form className="login-form-admin" onSubmit={formik.handleSubmit}>
             <Form.Input 
                 name="email"
                 placeholder="Email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.errors.email}
             />
 
             <Form.Input 
                 name="password"
                 placeholder="Password"
                 type="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={formik.errors.password}
             />
 
             <Button type="submit" content="Iniciar Sesión" primary fluid />
         </Form>
     )
 }
+
+function initialValues () {
+    return {
+        email: '',
+        password: ''
+    }
+};
+
+function validationSchema () {
+    return {
+        email: Yup.string().email(true).required(true),
+        password: Yup.string().required(true)
+    }
+};
