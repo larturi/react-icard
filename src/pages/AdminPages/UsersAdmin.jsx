6d6a4b6d340b70c9e0735ef9fb2fa19c1b609b66
lea.arturi <lea.arturi@gmail.com>
2021-11-09 18:52:05 -0300

    Basic Modal

--- src/pages/AdminPages/UsersAdmin.jsx
+++ src/pages/AdminPages/UsersAdmin.jsx
@@ -1,12 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Loader } from 'semantic-ui-react';
 import { HeaderPage, TableUsers } from '../../components/Admin'
+import { ModalBasic } from '../../components/Common'
 
 import { useAuth, useUser } from '../../hooks'
 
 export const UsersAdmin = () => {
+
+    const [ titleModal, setTitleModal ] = useState(null);
+    const [ showModal, setShowModal ] = useState(false);
+    const [ contentModal, setContentModal ] = useState(null);
 
     const { auth } = useAuth();
     const { loading, users, getUsers } = useUser();
@@ -15,18 +20,28 @@ export const UsersAdmin = () => {
         getUsers();
     }, []);
 
+    const openCloseModal = () => setShowModal((prev) => !prev);
+
     if(auth.me?.is_staff) {
         return (
             <>
                 <HeaderPage 
                     title="Usuarios"
                     btnTitle="Nuevo Usuario"
+                    btnClick={openCloseModal}
                 />
                 {loading ? (
                     <Loader active inline="centered">Cargando...</Loader>
                 ) : (
                     <TableUsers users={users} />
                 )}
+
+                <ModalBasic 
+                    show={showModal} 
+                    onClose={openCloseModal}
+                    title="Crear Usuario" 
+                    children={<h2>Contenido...</h2>}
+                />
             </>
         )
     } else {
