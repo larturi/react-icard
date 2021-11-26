334ff71bf8d883c105c2697c4c373d9fcf38d617
lea.arturi <lea.arturi@gmail.com>
2021-11-26 08:24:54 -0300

    Set dinamic color to orders depending status

--- src/components/Admin/Mesas/MesaDetailAdmin/OrderItemAdmin/OrderItemAdmin.jsx
+++ src/components/Admin/Mesas/MesaDetailAdmin/OrderItemAdmin/OrderItemAdmin.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Image } from 'semantic-ui-react';
+import classNames from 'classnames';
 
 import './OrderItemAdmin.scss';
 
@@ -8,14 +9,19 @@ export const OrderItemAdmin = (props) => {
     const { order } = props;
     const { title, image } = order.product_data;
 
+    const itemClass = classNames({
+        'order-item-admin': true,
+        [order.status.toLowerCase()]: true
+    });
+
     return (
-        <div className='order-item-admin'>
+        <div className={itemClass}>
             <div className='order-item-admin__time'>
                 {order.created_at}
             </div>
             <div className='order-item-admin__product'>
                 <Image src={image} />
-                <p>{title}</p>
+                <p>{title.toUpperCase()}</p>
             </div>
         </div>
     )
