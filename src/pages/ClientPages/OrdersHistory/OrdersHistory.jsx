cc67062164fdb9776e29eeea6f7dad8d4fe58d3b
lea.arturi <lea.arturi@gmail.com>
2021-12-05 14:10:07 -0300

    Modal pagar cuenta

--- src/pages/ClientPages/OrdersHistory/OrdersHistory.jsx
+++ src/pages/ClientPages/OrdersHistory/OrdersHistory.jsx
@@ -2,15 +2,19 @@
 
 import React, { useState, useEffect } from 'react';
 import { map, size, forEach} from 'lodash';
+import { Button } from 'semantic-ui-react';
 import { useParams } from 'react-router-dom';
 import { OrderHistoryItem } from '../../../components/Client';
 import { useOrder, useTable } from '../../../hooks';
+import { ModalConfirm } from '../../../components/Common';
 
 export const OrdersHistory = () => {
 
     const { loading, orders, getOrdersByTable } = useOrder();
     const { getTableByNumber } = useTable();
     const { tableNumber } = useParams();
+
+    const [showTypePayment, setShowTypePayment] = useState(false);
 
     useEffect(() => {
         (async () => {
@@ -27,11 +31,24 @@ export const OrdersHistory = () => {
 
             {loading ? <p>Cargando...</p> : (
                 <>
+                    {size(orders) > 0 && (
+                        <Button primary fluid onClick={() => setShowTypePayment(true)}>Pedir la cuenta</Button>
+                    )}
+
                     {map(orders, (order) => (
                         <OrderHistoryItem key={order.id} order={order} />
                     ))}
                 </>
             )}
+
+            <ModalConfirm
+                title="Pagar con Tarjeta o Efectivo"
+                show={showTypePayment}
+                onCloseText="Efectivo"
+                onClose={() => console.log('Pagar en eftvo')}
+                onConfirmText="Tarjeta"
+                onConfirm={() => console.log('Pagar con tarjeta')}
+            />
         </div>
     )
 }
