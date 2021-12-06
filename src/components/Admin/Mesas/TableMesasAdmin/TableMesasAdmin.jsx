f0b6f62e1831cc9836500dfab951a93317bb2e78
lea.arturi <lea.arturi@gmail.com>
2021-12-06 11:10:27 -0300

    Genera QR con link a la mesa

--- src/components/Admin/Mesas/TableMesasAdmin/TableMesasAdmin.jsx
+++ src/components/Admin/Mesas/TableMesasAdmin/TableMesasAdmin.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Table, Button, Icon } from 'semantic-ui-react';
 import { map } from 'lodash';
+import QRCode from 'qrcode.react';
+import { ModalBasic } from '../../../Common';
 
 import './TableMesasAdmin.scss';
 
@@ -8,38 +10,66 @@ export const TableMesasAdmin = (props) => {
 
     const { tables, updateTable, deleteTable } = props;
 
-    return (
-        <Table className="table-tables-admin">
-            <Table.Header>
-                <Table.Row>
-                    <Table.HeaderCell>Numero de Mesa</Table.HeaderCell>
-                    <Table.HeaderCell></Table.HeaderCell>
-                </Table.Row>
-            </Table.Header>
+    const [showModal, setShowModal] = useState(false);
+    const [contentModal, setContentModal] = useState(null);
 
-            <Table.Body>
-                {map(tables, (table, index) => (
-                    <Table.Row key={index}>
-                        <Table.Cell>Mesa #{table.number}</Table.Cell>
-                        <Actions 
-                            table={table} 
-                            updateTable={updateTable} 
-                            deleteTable={deleteTable} 
-                        />
+    const openCloseModal = () => setShowModal((prevState) => !prevState);
+
+    const showQr = (table) => {
+        setContentModal(
+            <div style={{ textAlign: "center" }}>
+                <QRCode value={`${window.location.origin}/client/${table.number}`} />
+            </div>
+        );
+        openCloseModal();
+    }
+
+    return (
+        <>
+            <Table className="table-tables-admin">
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Numero de Mesa</Table.HeaderCell>
+                        <Table.HeaderCell></Table.HeaderCell>
                     </Table.Row>
-                ))}
-            </Table.Body>
-            
-        </Table>
+                </Table.Header>
+
+                <Table.Body>
+                    {map(tables, (table, index) => (
+                        <Table.Row key={index}>
+                            <Table.Cell>Mesa #{table.number}</Table.Cell>
+                            <Actions 
+                                table={table} 
+                                updateTable={updateTable} 
+                                deleteTable={deleteTable} 
+                                showQr={showQr}
+                            />
+                        </Table.Row>
+                    ))}
+                </Table.Body>
+            </Table>
+
+            <ModalBasic 
+                show={showModal}
+                onClose={openCloseModal}
+                title="Código QR"
+                size="mini"
+                children={contentModal}
+            />
+        </>
     )
 };
 
 
 function Actions(props) {
-    const { table, updateTable, deleteTable } = props;
+    const { table, updateTable, deleteTable, showQr } = props;
 
     return (
         <Table.Cell textAlign="right">
+            <Button icon onClick={() => showQr(table)}>
+                <Icon name="qrcode" />
+            </Button>
+
             <Button icon onClick={() => updateTable(table)}>
                 <Icon name="pencil" />
             </Button>
